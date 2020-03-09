9addcd1854d5573a6d49510b710cc830b0639a1f
Aenqau <cauthraydon@gmail.com>
2020-03-09 18:09:51 +0200

    Adjusted the readme and config

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -9,26 +9,26 @@ module.exports = {
   // },
 
   development: {
-    database: 'school',
-    username: 'postgres',
-    password: process.env.DB_PASS,
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  },
-
-  test: {
-    database: 'school',
-    username: 'postgres',
-    password: null,
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  },
-
-  production: {
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
     dialect: 'postgres'
-  }
+  },
+
+  test: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+
+  // production: {
+  //   database: process.env.DB_NAME,
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   host: process.env.DB_HOST,
+  //   dialect: 'postgres'
+  // }
 };
