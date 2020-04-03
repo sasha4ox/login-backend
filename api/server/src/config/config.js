3119236d3135418d65f5e0cac3e29ca1e8d8a68f
sasha4ox <sasha4ox@gmail.com>
2020-04-03 18:28:35 +0300

    dataBase

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -14,7 +14,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: process.env.DB_URL,
   },
   test: {
     database: process.env.DB_NAME,
