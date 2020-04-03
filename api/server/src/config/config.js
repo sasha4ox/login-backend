ae59a98208bc856e4fe06ea397ea813f88ebb632
sasha4ox <sasha4ox@gmail.com>
2020-04-03 19:57:56 +0300

    heroku

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -15,8 +15,11 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     dialect: 'postgres',
-    use_env_variable: process.env.DB_URL,
+    use_env_variable: 'DATABASE_URL',
   },
   test: {
     database: process.env.DB_NAME,
