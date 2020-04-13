5176ce0aea9fe974600d843505ec30a50c4fb10d
sasha4ox <sasha4ox@gmail.com>
2020-04-13 17:19:17 +0300

    heroku drop unnecessary table

--- api/server/src/migrations/20200327123841-create-test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  up: queryInterface => {
-    return queryInterface.createTable('tests', {});
-  },
-  down: queryInterface => {
-    return queryInterface.dropTable('tests');
-  },
-};
