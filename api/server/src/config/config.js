51f2eafc8df2406ceeb36011637f551fd836acf4
sasha4ox <sasha4ox@gmail.com>
2020-04-15 11:39:02 +0300

    h

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -28,12 +28,4 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
-
-  // production: {
-  //   database: process.env.DB_NAME,
-  //   username: process.env.DB_USER,
-  //   password: process.env.DB_PASS,
-  //   host: process.env.DB_HOST,
-  //   dialect: 'postgres'
-  // }
 };
